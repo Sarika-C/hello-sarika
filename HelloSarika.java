4d30b7bf5b20d95e511df5fbd7d17999d6652277
Sarika Chawla <Sarika-C@users.noreply.github.com>
2019-07-27 17:38:49 -0400

    Add files via upload

---
+++ HelloSarika.java
@@ -0,0 +1,17 @@
+/*
+ * Prints a simple greeting
+ * Illustrates a simple program for novice students
+ */
+
+/*
+ * Written by Sarika Chawla
+ */
+
+public class HelloSarika {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Sarika!\nHow was your day?");
+		// System.out.print(How was your day?);
+	}
+	
+}
